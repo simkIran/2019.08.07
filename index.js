0e7637b16998d4f3ce0781f8888a4769f2e2b774
Kirandeep Kaur <741453@student.tor.loc>
2019-08-07 16:18:48 -0400

    commit

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-const http=require('http');
+/*const http=require('http');
 const site=http.createServer(function(req,res){
     console.log('Hello World');
     //res.write('YeaH...Go HTTP!!!');
     res.setHeader('Content-type','text/html');
     console.log(req.rawHeaders)
     res.end('<h1>Hello Guys..!!!</h1>')
+});*/
+       
+
+
+var express=require('express');
+var app =express();
+app.get('/',function(req,res){
+    res.send('Hello World')
 });
 
-site.listen(3000);       
+app.listen(3000)
+
+site.listen(3000);
