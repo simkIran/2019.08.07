c83f578024dab5173c4d2eeef57d4f5410771196
Kirandeep Kaur <741453@student.tor.loc>
2019-08-07 15:31:50 -0400

    third commit

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const http=require('http');
 const site=http.createServer(function(req,res){
     console.log('Hello World');
+    res.write('YeaH...Go HTTP!!!');
+    res.setHeader('what should I do here..?');
+    res.end('<h1>Hello Guys</h1>')
 });
 
 site.listen(3000);
