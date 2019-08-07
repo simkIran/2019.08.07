6dbb819525ea5c9b85cd448f148d561925f7cba9
Kirandeep Kaur <741453@student.tor.loc>
2019-08-07 16:13:42 -0400

    fourth commit

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const http=require('http');
 const site=http.createServer(function(req,res){
     console.log('Hello World');
-    res.write('YeaH...Go HTTP!!!');
-    res.setHeader('what should I do here..?');
-    res.end('<h1>Hello Guys</h1>')
+    //res.write('YeaH...Go HTTP!!!');
+    res.setHeader('Content-type','text/html');
+    console.log(req.rawHeaders)
+    res.end('<h1>Hello Guys..!!!</h1>')
 });
 
-site.listen(3000);
+site.listen(3000);       
